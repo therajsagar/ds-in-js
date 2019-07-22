b4e06f5f1c67499f857e244c0bb8b19831d0878d
Raj Sagar <therajsagar@gmail.com>
2019-07-22 10:16:11 +0530

    changes in BST insert

--- BST.js
+++ BST.js
@@ -14,63 +14,71 @@ class BinarySearchTree {
 
 BinarySearchTree.prototype.insert = function(data) {
   const node = new Node(data);
+
   let root = this.root;
   if (!root) {
     this.root = node;
     return;
   }
-  let current = root;
-  while (current) {
-    if (data < root.value) {
+
+  const search = current => {
+    if (data < current.value) {
       if (!current.left) {
         current.left = node;
         return;
       } else {
-        current = current.left;
+        search(current.left);
       }
-    } else {
+    } else if (data > current.value) {
       if (!current.right) {
         current.right = node;
         return;
       } else {
-        current = current.right;
+        search(current.right);
       }
     }
-  }
+  };
+  search(root);
 };
 
-BinarySearchTree.prototype.remove = function(root = this.root, data) {
-  if (!this.root) {
-    console.log('Empty Tree');
-    return;
-  }
-  if (data < root.value) {
-    root.left = this.remove(root.left, data);
-    return root;
-  } else if (data > root.value) {
-    root.right = this.remove(root.right, data);
-    return root;
-  } else {
-    if (!root.left && !root.right) {
-      root = null;
-      return root;
-    }
-    if (!root.left) {
-      return root.right;
-    }
-    if (!root.right) {
-      return root.left;
-    }
-
-    let current = root.right;
-    while (current.left) {
-      current = current.left;
-    }
-    root.data = current.data;
-    root.right = this.remove(root.right, current.data);
-    return root;
-  }
-};
+// BinarySearchTree.prototype.remove = function(data, root = this.root) {
+//   if (!this.root) {
+//     console.log('Empty Tree');
+//     return;
+//   }
+//   let current = root;
+//   while (current) {
+//     if (data === current.value) {
+//       if (!current.left && !current.right) {
+//         current = null;
+//         return;
+//       } else if (!current.left) {
+//         current = current.right;
+//       } else if (!current.right) {
+//         current = current.left;
+//       } else {
+//         let piv = current.right;
+//         while (piv.left) {
+//           piv = piv.left;
+//         }
+//         current = piv;
+//         piv = null;
+//       }
+//     } else if (data < current.value) {
+//       if (!current.left) {
+//         return 'Element Not Found.';
+//       } else {
+//         current = current.left;
+//       }
+//     } else {
+//       if (!current.right) {
+//         return 'Element Not Found.';
+//       } else {
+//         current = current.right;
+//       }
+//     }
+//   }
+// };
 
 BinarySearchTree.prototype.getMinNode = function() {
   let root = this.root;
@@ -128,29 +136,61 @@ BinarySearchTree.prototype.contains = function(data) {
   }
 };
 
-BinarySearchTree.prototype.inorder = function(root = this.root) {
-  if (!this.root) {
-    return 'Empty Tree';
-  }
-  root.left && this.inorder(root.left);
-  console.log(root.value);
-  root.right && this.inorder(root.right);
-};
-
-BinarySearchTree.prototype.preorder = function(root = this.root) {
+BinarySearchTree.prototype.inorder = function() {
+  let root = this.root;
   if (!root) {
     return 'Empty Tree';
   }
-  console.log(root.value);
-  root.left && this.preorder(root.left);
-  root.right && this.preorder(root.right);
+  let output = [];
+  const traverse = node => {
+    node.left && traverse(node.left);
+    output.push(node.value);
+    node.right && traverse(node.right);
+  };
+  traverse(root);
+  return output;
 };
 
-BinarySearchTree.prototype.postorder = function(root = this.root) {
+BinarySearchTree.prototype.preorder = function() {
+  let root = this.root;
   if (!root) {
     return 'Empty Tree';
   }
-  root.left && this.postorder(root.left);
-  root.right && this.postorder(root.right);
-  console.log(root.value);
+  let output = [];
+  const traverse = node => {
+    output.push(node.value);
+    node.left && traverse(node.left);
+    node.right && traverse(node.right);
+  };
+  traverse(root);
+  return output;
+};
+
+BinarySearchTree.prototype.postorder = function() {
+  let root = this.root;
+  if (!root) {
+    return 'Empty Tree';
+  }
+  let output = [];
+  const traverse = node => {
+    node.left && traverse(node.left);
+    node.right && traverse(node.right);
+    output.push(node.value);
+  };
+  traverse(root);
+  return output;
+};
+
+BinarySearchTree.prototype.BFS = function() {
+  let root = this.root;
+  let result = [];
+  let queue = [root];
+
+  while (queue.length > 0) {
+    let current = queue.shift();
+    result.push(current);
+    current.left && queue.push(current.left);
+    current.right && queue.push(current.right);
+  }
+  return result;
 };
